cad8f660fda5edaff255a75d1af6e8b26e0d1c0c
Aakash Chauhan <aakash.chauhan@biz2x.com>
2025-10-01 17:02:00 +0530

    started with auth

---
+++ backend/routes/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/Users");
+
+
+router.post("/register", async(req, res) => {
+
+})
+
+router.post("/login", async(req, res) => {
+    
+})
