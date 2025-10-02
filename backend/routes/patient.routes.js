2c4be116cafeea114b1c1214a5c25aabe00e2c91
aakash1408 <aakashchauhan14082003@gmail.com>
2025-10-02 13:39:39 +0530

    added some routes and auth middleware

---
+++ backend/routes/patient.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const Patients = require("../models/Patients");
+const auth = require("../middleware/auth");
+
+router.get("/", auth, async(req,res) => {
+    try{
+        const patients = await Patients.find().populate("user", ["name", "email"]);
+        res.json(patients);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send("Server errors")
+    }
+})
--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,13 @@ app.use(cors());
 
 connectTodb();
 
-app.use("/api/auth", require());
-app.use("/api/patient", require());
-app.use("/api/doctors", require());
-app.use("/api/appointments". require());
-app/use("api/medical-records", require());
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/patients", require("./routes/patient.routes"));
+app.use("/api/doctors", require("./routes/doctor.routes"));
+app.use("/api/appointments", require("./routes/appointment.routes"));
+app.use("/api/medical-records", require("./routes/record.routes"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
